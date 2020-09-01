05204622e15d35094e4a230eb330dca924425a25
Nolaine Crusher <nolainecrusher@gmail.com>
2020-08-31 22:10:59 -0700

    fixed naming issue

--- Tests/SwiftLanguageAndLocaleCodesTests/SwiftLanguageAndLocaleCodesTests.swift
+++ Tests/SwiftLanguageAndLocaleCodesTests/SwiftLanguageAndLocaleCodesTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import LanguageAndLocaleCodes
 
-final class LanguageAndLocaleCodesTests: XCTestCase {
+final class SwiftLanguageAndLocaleCodesTests: XCTestCase {
 //    func testExample() {
 //        // This is an example of a functional test case.
 //        // Use XCTAssert and related functions to verify your tests produce the correct
